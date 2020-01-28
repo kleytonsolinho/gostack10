a82d2afeea5b9651f30758a69362335249a724d5
Kleyton Solinho <kleytonsolinho@gmail.com>
2020-01-28 17:25:03 -0300

    Exempla Test Query Params

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const server = express();
 
+//Query Params = ?teste=1
+//Route Params = /users/1
+//Request Body = { "name": "Kleyton", "email": "teste@teste.com.br" }
+
 server.get('/teste', (req, res) => {
-  return res.json({ message: 'Hello World!' });
+  const nome = req.query.nome;
+
+  return res.json({ message: `Hello ${nome}` });
 })
 
 server.listen(3000);
