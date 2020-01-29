3b851af77b5152c1d27151efbaa7d220f4844e75
Kleyton Solinho <kleytonsolinho@gmail.com>
2020-01-29 15:22:23 -0300

    CRUD config

--- modulo01/index.js
+++ modulo01/index.js
@@ -1,17 +1,49 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
 
 //Query Params = ?teste=1
 //Route Params = /users/1
 //Request Body = { "name": "Kleyton", "email": "teste@teste.com.br" }
 
+//CRUD - Create, Read, Update, Delete
+
 const users = ["Lucas", "Robson", "João"];
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+})
 
 server.get('/users/:index', (req, res) => {
   const { index } = req.params;
 
   return res.json(users[index]);
 })
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
+});
 
 server.listen(3000);
