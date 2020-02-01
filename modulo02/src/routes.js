94eedb3acec4230ce377d9bb973868ac42ba9308
Kleyton Solinho <kleytonsolinho@gmail.com>
2020-02-01 11:16:44 -0300

    Migration + Models + LoaderBD

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/Users';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Rocketseat!' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandes',
+    email: 'diego@rocketseat.com.br',
+    password_hash: '1241934905',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
