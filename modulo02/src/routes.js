9cea0a5bbd8e563b5e8c4ddd039bf5952de57719
Kleyton Solinho <kleytonsolinho@gmail.com>
2020-02-02 15:33:34 -0300

    gerando hash password

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/Users';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Diego Fernandes',
-    email: 'diego@rocketseat.com.br',
-    password_hash: '1241934905',
-  });
-
-  return res.json(user);
-}
+routes.post('/users', UserController.store);
 
 export default routes;
