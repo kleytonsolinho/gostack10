c26b8bc53ac577dce23abbf28b636e90f2b28118
Kleyton Solinho <kleytonsolinho@gmail.com>
2020-02-10 15:35:28 -0300

    nodemailer

---
+++ modulo03/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '167458c63f56e2',
+    pass: '765d7d03da054f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
