6f8a6d71a438feb5939e4c5439b76a2fa84429ca
Kleyton Solinho <kleytonsolinho@gmail.com>
2020-07-06 21:31:33 -0300

    modulo07 rocketshoes json

---
+++ modulo07/src/pages/Cart/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
+
+function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-puma-lqdcell-optic-sheer-masculino/34/NWG-1546-234/NWG-1546-234_zoom2.jpg?ts=1593538359&ims=326x"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tenis muito Legal</strong>
+              <span>R$459,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readonly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$919,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>Total</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
+
+export default Cart;
